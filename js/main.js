ed94ab919e00c9c402878a5bbadb7ce139e53d40
ra - our lady <rawalvarez731@gmail.com>
2023-03-27 12:35:57 -0400

    Add

--- js/main.js
+++ js/main.js
@@ -1,79 +1,119 @@
-const socket = io("http://18.119.139.37:5000");
-const usersList = document.getElementById('users');
-const registerForm = document.getElementById('register-form');
+const socket = io('http://localhost:3000');
 
-// Handle updates to the list of online users
-socket.on('online-users', (users) => {
-    usersList.innerHTML = '';
-    users.forEach((user) => {
-        const li = document.createElement('li');
-        li.textContent = user;
-        usersList.appendChild(li);
+// Login form
+const loginForm = document.querySelector('#login-form');
+const loginButton = document.querySelector('#login-btn');
+const loginUsername = document.querySelector('#login-username');
+const loginPassword = document.querySelector('#login-password');
+
+// Register form
+const registerForm = document.querySelector('.register-form');
+const registerButton = document.querySelector('#register-button');
+
+// Chat form
+const chatForm = document.querySelector('#chat-form');
+const chatMessage = document.querySelector('#message-input');
+const chatMessages = document.querySelector('#messages');
+const chatUserList = document.querySelector('#user-list');
+const chatLogoutButton = document.querySelector('#logout-btn');
+const sendButton = document.querySelector('#send-btn')
+
+// Login form submission
+loginButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+    
+    const username = loginUsername.value;
+    const password = loginPassword.value;
+    
+    const response = await fetch('http://localhost:3000/chat/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
     });
+
+    if (response.ok) {
+        loginForm.classList.add('hidden');
+        chatForm.classList.remove('hidden');
+        registerForm.classList.add('hidden');
+        
+        socket.emit('join', username);
+    } else {
+        alert('Invalid username or password');
+    }
 });
 
-// Handle registration form submission
-registerForm.addEventListener('submit', (event) => {
+// Chat form submission
+sendButton.addEventListener('click', (event) => {
     event.preventDefault();
-    const formData = new FormData(registerForm);
-    const data = {
-        username: formData.get('username'),
-        email: formData.get('email'),
-        password: formData.get('password')
-    };
-    fetch('/chat/register', {
+    
+    const message = chatMessage.value;
+    
+    socket.emit('sendMessage', { username: document.cookie, message: message });
+    
+    chatMessage.value = '';
+});
+
+// Logout button click
+chatLogoutButton.addEventListener('click', async () => {
+    const response = await fetch('http://localhost:3000/chat/logout', {
+        method: 'POST',
+    });
+
+    if (response.ok) {
+        loginForm.classList.remove('hidden');
+        registerForm.classList.remove('hidden');
+        chatForm.classList.add('hidden');
+        socket.disconnect();
+    }
+});
+
+// Register button click
+registerButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    const username = document.querySelector('#register-username').value;
+    const email = document.querySelector('#register-email').value;
+    const password = document.querySelector('#register-password').value;
+    
+    fetch('http://localhost:3000/chat/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify({ username, email, password })
     })
-    .then((response) => response.json())
-    .then((result) => {
-        if (result.status === 'success') {
-            console.log('Registration successful!');
+    .then(response => {
+        if (response.ok) {
+            // Registration was successful
+            alert('Registration successful!');
         } else {
-            console.error(result.message);
+            // Registration failed
+            response.json().then(data => {
+                alert(data.message);
+            });
         }
     })
-    .catch((err) => {
-        console.error(err);
+    .catch(error => {
+        console.error(error);
+        alert('Registration failed');
     });
 });
 
-//--------------------------------------------------------------------------------------//
-//                                         Socket.io                                    //
-//--------------------------------------------------------------------------------------//
-io.on('connection', (socket) => {
-    console.log('User connected');
-    
-    // Add the user to the online users list
-    pool.query('INSERT INTO online_users (user_id) VALUES (?)', [socket.request.session.userId]);
-    
-    // Emit the list of online users to all clients
-    pool.query('SELECT username FROM users WHERE id IN (SELECT user_id FROM online_users)', (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const users = rows.map((row) => row.username);
-            io.emit('online-users', users);
-        }
+// Add user to user list
+socket.on('userList', (users) => {
+    chatUserList.innerHTML = '';
+    users.forEach((user) => {
+        const li = document.createElement('li');
+        li.textContent = user.username;
+        chatUserList.appendChild(li);
     });
-    
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-        
-        // Remove the user from the online users list
-        pool.query('DELETE FROM online_users WHERE user_id=?', [socket.request.session.userId]);
-        
-        // Emit the list of online users to all clients
-        pool.query('SELECT username FROM users WHERE id IN (SELECT user_id FROM online_users)', (err, rows) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const users = rows.map((row) => row.username);
-                io.emit('online-users', users);
-            }
-        });
-    });
+});
+
+// Add message to message list
+socket.on('message', (data) => {
+    const li = document.createElement('li');
+    li.textContent = `${data.username}: ${data.message}`;
+    chatMessages.appendChild(li);
 });
--- server.js
+++ server.js
@@ -1,56 +1,176 @@
 const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
 const mariadb = require('mariadb');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const socketio = require('socket.io');
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+const server = require('http').createServer(app);
+const io = socketio(server, {
+	cors: {
+		origin: '*',
+	},
+});
 
+// MariaDB Connection Pool
 const pool = mariadb.createPool({
 	host: 'localhost',
 	user: 'root',
-	password: 'newpassword',
-	database: 'chatapp'
+	password: '123',
+	database: 'chatapp',
+	connectionLimit: 5,
 });
 
+// Middleware
 app.use(express.json());
-app.use(express.static(__dirname + '/dist'));
+app.use(cors());
 
-app.get('/users', async (req, res) => {
-	const conn = await pool.getConnection();
-	const rows = await conn.query('SELECT username FROM users');
-	conn.release();
-	res.json(rows);
-});
-
+// Register API
 app.post('/chat/register', async (req, res) => {
 	const { username, email, password } = req.body;
-	const conn = await pool.getConnection();
+	
+	// Check if username already exists
+	const usernameExists = await pool.query(
+		`SELECT * FROM users WHERE username = ?`,
+		[username]
+	);
+		
+	if (usernameExists.length > 0) {
+		return res.status(400).json({ message: 'Username already exists' });
+	}
+
+	// Check if email already exists
+	const emailExists = await pool.query(
+		`SELECT * FROM users WHERE email = ?`,
+		[email]
+	);
+
+	if (emailExists.length > 0) {
+		return res.status(400).json({ message: 'Email already exists' });
+	}
+
+	// Check password length
+	if (password.length < 6) {
+		return res
+		.status(400)
+		.json({ message: 'Password must be at least 6 characters long' });
+	}
+
+	// Hash password
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	// Insert user into database
+	const result = await pool.query(
+	`INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
+	[username, email, hashedPassword]
+	);
+
+	if (result.affectedRows === 1) {
+		return res.status(201).json({ message: 'User registered successfully' });
+	} else {
+		return res.status(500).json({ message: 'Something went wrong' });
+	}
+});
+
+// Login API
+app.post('/chat/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	console.log(username + ' ' + password);
+
+	// Check if user exists
+	const user = await pool.query(
+		`SELECT * FROM users WHERE username = ?`,
+		[username]
+	);
+
+	if (user.length === 0) {
+		return res.status(401).json({ message: 'Invalid username or password' });
+	}
+
+	// Compare passwords
+	const isPasswordCorrect = await bcrypt.compare(password, user[0].password);
+
+	if (!isPasswordCorrect) {
+		return res.status(401).json({ message: 'Invalid username or password' });
+	}
+
+	// Create and sign JWT
+	const token = jwt.sign(
+		{
+			id: user[0].id,
+			username: user[0].username,
+		},
+		'secret',
+		{ expiresIn: '1h' }
+		);
+		
+		// Return JWT as a cookie
+		res.cookie('jwt', token, {
+			httpOnly: true,
+			maxAge: 60 * 60 * 1000,
+	});
+		
+	return res.status(200).json({ message: 'Logged in successfully' });
+});
+
+// Logout API
+app.post('/chat/logout', async (req, res) => {
+	// Clear JWT cookie
+	res.clearCookie('jwt');
+	
+	return res.status(200).json({ message: 'Logged out successfully' });
+});
+
+// Chat API
+app.get('/chat', async (req, res) => {
+	// Verify JWT
+	const token = req.cookies.jwt;
+	
+	if (!token) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
+	
 	try {
-		const userExists = await conn.query(
-			'SELECT COUNT(*) as count FROM users WHERE username=? OR email=?',
-			[username, email]
-			);
-			if (userExists[0].count > 0) {
-				throw new Error('Username or email already exists');
-			}
-			if (password.length < 6) {
-				throw new Error('Password must be at least 6 characters long');
-			}
-			await conn.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [
-				username,
-				email,
-				password
-			]);
-			res.json({ status: 'success' });
-		} catch (err) {
-			res.status(400).json({ status: 'fail', message: err.message });
-		} finally {
-			conn.release();
+		const decoded = jwt.verify(token, 'secret');
+		req.user = decoded;
+		return res.status(200).json({ message: 'Authorized' });
+	} catch (err) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
+});
+
+// Socket.io
+let users = [];
+
+io.on('connection', (socket) => {
+	console.log('User connected');
+	
+	// Add user to users array
+	socket.on('join', (username) => {
+		const user = { id: socket.id, username };
+		users.push(user);
+		io.emit('userList', users);
+	});
+	
+	// Remove user from users array
+	socket.on('disconnect', () => {
+		const index = users.findIndex((user) => user.id === socket.id);
+		if (index !== -1) {
+			users.splice(index, 1);
+			io.emit('userList', users);
 		}
 	});
 	
-	server.listen(PORT, () => {
-		console.log(`Server started on port ${PORT}`);
+	// Send message to all users
+	socket.on('sendMessage', (message) => {
+		console.log(message);
+		io.emit('message', { user: socket.id, message });
 	});
-	
+});
+
+// Start server
+server.listen(3000, () => {
+	console.log('Server started on port 3000');
+});
