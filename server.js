a83c1f29bc334aa36c3a1c74b78b64538ed2b159
ra - our lady <rawalvarez731@gmail.com>
2023-03-26 20:07:49 -0400

    Add

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const mariadb = require('mariadb');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const pool = mariadb.createPool({
 	host: 'localhost',
